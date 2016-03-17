676bdc39cb6b42f97b0a7a572e6df80375f83f18
Ronando Long <ronando.long@gmail.com>
2016-03-17 15:06:51 -0700

    Initial commit, files created, radio button form is complete.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+
+// Jumbotron image
+  var jumboHeight = $('.jumbotron').outerHeight();
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
+
+    $(window).scroll(function(e){
+        parallax();
+    });
+});
